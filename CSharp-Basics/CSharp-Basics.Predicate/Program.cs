430d8bdec466e30090a98b18a2721f552ceadeb6
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2021-02-09 13:12:19 +0300

    Add Delegate

---
+++ CSharp-Basics/CSharp-Basics.Predicate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Basics.Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
