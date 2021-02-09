01213ad95e1a6b522b320bb30671d40a6a69506a
Sadettin Kepenek <Sadettin.Kepenek@hotmail.com>
2021-02-09 14:21:45 +0300

    Add Partial Class

---
+++ CSharp-Basics/CSharp-Basics.PartialClass/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp_Basics.PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("John", "Doe");
+            var fullName = employee.GetFullName();
+            Console.WriteLine(fullName);
+        }
+    }
+}
